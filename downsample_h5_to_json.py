4884cf44d3669a935120d5a403bb2f63b37e388d
Antony Siahaan <als88@login-p-1.data.cluster>
2025-07-07 17:23:33 +0100

    it's working, using the step and hr time as reference, need to be mapped to acc timestamp

--- downsample_h5_to_json.py
+++ downsample_h5_to_json.py
@@ -7,40 +7,68 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
+def transform_path(path: str) -> str:
+    parts = path.strip().split('/')
+    if len(parts) < 5:
+        raise ValueError("Path must contain at least 5 components")
+
+    short_dir, uuid, dataset, compound_part, filename = parts[-5:]
+    
+    compound_parts = compound_part.split(':')
+    #compound_parts = compound_part.split('-')
+    compound_joined = '-'.join(compound_parts)
+    id_part = compound_parts[-1]
+    
+    file_base = os.path.splitext(filename)[0]
+    
+    #return id_part, f"{id_part}_{short_dir}-{uuid}-{dataset}-{compound_joined}_{file_base}.json"
+    return id_part, f"{id_part}_{short_dir}-{uuid}-{compound_joined}_{file_base}.json"
+
+
 def to_datetime_index(timestamps):
     return pd.to_datetime(timestamps, unit='s')
 
 def process_file(file_path, output_path):
     with h5py.File(file_path, 'r') as f:
         # Extract ID from group
-        device_id = list(f.keys())[0]
-        base_path = f"{device_id}"
+        rootgrp = list(f.keys())[0]
+        base_path = f"{rootgrp}"
 
         acc_path = f"{base_path}/Accelerometer/Ch_0/Data"
         t_acc = f[acc_path + "/t"][:]
 
         hr_path = f"{base_path}/Heart_rate/Ch_0/Data"
-        t_hr = f[hr_path + "/t"][:]
-        hr = f[hr_path + "/heart_rate"][:]
+        if 'Heart_rate' not in f[base_path]:
+            # Create empty DataFrame with null heart_rate for the same index as acc timestamps
+            hr_minute = pd.DataFrame({'heart_rate': pd.Series(dtype='Int64')}, index=to_datetime_index(t_acc))
+            hr_minute = hr_minute.resample('1T').asfreq()
+        else:
+            t_hr = f[hr_path + "/t"][:]
+            hr = f[hr_path + "/heart_rate"][:]
+            # Heart rate
+            df_hr = pd.DataFrame({'heart_rate': hr}, index=to_datetime_index(t_hr))
+            hr_minute = df_hr.resample('1T').mean()
 
-        
-        # Heart rate
-        df_hr = pd.DataFrame({'heart_rate': hr}, index=to_datetime_index(t_hr))
-        hr_minute = df_hr.resample('1T').mean()
 
-        step_path = f"{base_path}/Step_count/Ch_0/Data"
-        t_step = f[step_path + "/t"][:]
-        #steps = f[step_path + "/steps"][:]
-
-        # Step detection, extracted from cummulative stepCount data which increases on detecting a new step
-        df_steps = pd.DataFrame({'step_count':1}, index=to_datetime_index(t_step))  
-        steps_minute = df_steps.resample('1T').sum()
+        # Set the 'step_count' column as null in steps_minute dataframe if Step_count group is not found in the HDF5 f object
+        if 'Step_count' not in f[base_path]:
+            steps_minute = pd.DataFrame({'step_count': pd.Series(dtype='Int64')}, index=hr_minute.index)
+        else:
+            step_path = f"{base_path}/Step_count/Ch_0/Data"
+            t_step = f[step_path + "/t"][:]
+            #steps = f[step_path + "/steps"][:]
+            
+            # Step detection, extracted from cummulative stepCount data which increases on detecting a new step
+            df_steps = pd.DataFrame({'step_count':1}, index=to_datetime_index(t_step))  
+            steps_minute = df_steps.resample('1T').sum()
+            # Replace NaN in column 'step' with 0, I think this is no longer necessary ?
+            steps_minute['step_count'] = steps_minute['step_count'].fillna(0)
 
         # Combine
         #combined = pd.concat([acc_minute, hr_minute, steps_minute], axis=1)
         combined = pd.concat([hr_minute, steps_minute], axis=1)        
-        # Replace NaN in column 'step' with 0, I think this is no longer necessary ?
-        combined['step_count'] = combined['step_count'].fillna(0)
+        ## Replace NaN in column 'step' with 0, I think this is no longer necessary ?
+        #combined['step_count'] = combined['step_count'].fillna(0)
 
         # Format timestamp with :00 seconds
         combined.reset_index(inplace=True)
@@ -49,6 +77,8 @@ def process_file(file_path, output_path):
 
         combined['step_count'] = combined['step_count'].round().astype('Int64')  # using Int64 preserves Null        
         combined['heart_rate'] = combined['heart_rate'].round().astype('Int64')
+
+        device_id, filename = transform_path(file_path)
 
         result = {
             "deviceid": str(device_id),
@@ -66,8 +96,10 @@ def process_file(file_path, output_path):
 
         # Write to JSON
         #filename = os.path.basename(file_path).replace('.hdf5', '.json')
-        filename = str(Path(file_path).with_suffix('.json').name)
+        #filename = str(Path(file_path).with_suffix('.json').name)
         out_file = os.path.join(output_path, filename)
+        #out_file = transform_path(file_path)
+        print(f'Writing to : {out_file}')
         with open(out_file, 'w') as f_out:
             json.dump(result, f_out, indent=2)
 
